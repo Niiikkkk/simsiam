4c4dc2450d7d43934d705c3eb36b80260ee3e1b8
nicholas.berardo <berardonicholas5@gmail.com>
2024-10-02 09:39:56 +0200

    run

--- extract_backbone.py
+++ extract_backbone.py
@@ -1,10 +1,10 @@
 import torch
 
-model = torch.load('checkpoint_0099.pth.tar', map_location="cpu")
+model = torch.load('downloaded/checkpoint_0099.pth.tar', map_location="cpu")
 new_state_dict = {}
 for name,weights in model["state_dict"].items():
     print(name)
     if name.startswith("module.encoder."):
         name = name.replace("module.encoder.", "")
         new_state_dict[name] = weights
-#torch.save(new_state_dict, 'resnet50.pth')
+torch.save(new_state_dict, 'resnet50_downloaded.pth')
